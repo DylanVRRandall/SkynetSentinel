f8dcb005ee680cdb0da56376f7b3a36dc19c857e
DESKTOP-4TA486A\Dylan <randalldylan101@gmail.com>
2022-06-20 23:24:18 +0200

    wip

--- web/src/main/java/com/google/randalldylan101/sentinel/rest/RobotCPUController.java
+++ web/src/main/java/com/google/randalldylan101/sentinel/rest/RobotCPUController.java
@@ -5,8 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces; 
 import javax.ws.rs.Consumes; 
 import javax.ws.rs.core.MediaType;
-import com.google.randalldylan101.sentinel.beans.RobotCPU;
-import com.google.randalldylan101.sentinel.entities.Survivor;
+import com.google.randalldylan101.sentinel.service.RobotCPUService;
+import java.io.IOException;
 
 @Path("/robots")
 public class RobotCPUController
@@ -15,8 +15,8 @@ public class RobotCPUController
    @GET 
    @Path("/get") 
    @Produces(MediaType.APPLICATION_JSON)
-   public void get()
+   public void get() throws IOException
    {
-
+      new RobotCPUService().get();
    }  
 }

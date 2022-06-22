789cf0cf74a80baae8c312f8449f8885a2bbf148
DESKTOP-4TA486A\Dylan <randalldylan101@gmail.com>
2022-06-22 22:44:55 +0200

    fixes

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-REM although the path has been set on my windows machine it does not seem to pick it up here so hence the hardcoded path to mvn && ^
 C:\apache-maven-3.8.6\bin\mvn -f web\pom.xml clean install && ^
-
 copy web\target\skynetsentinelweb.war docker\skynetsentinelweb.war && ^
 cd docker && ^
 docker build . -t skynet && ^
